a0007008a6253653bb143c019424b95b294f3af9
Calvin <me@iCalvin.dev>
2024-10-13 12:06:33 -0400

    Bring EditorView up to spec

--- Sources/MarkdownEditor/MarkdownEditorView.swift
+++ Sources/MarkdownEditor/MarkdownEditorView.swift
@@ -8,19 +8,63 @@
 import SwiftUI
 
 @available(iOS 18.0, *)
-public struct MarkdownEditorView: View {
+public struct MarkdownEditorView<C: View>: View {
     @Binding var text: String
     @Binding var selection: TextSelection?
     
+    @ViewBuilder
+    let toolbarBuilder: (() -> C)?
+    
     let selectionHandler: SelectionHandler = .init()
+    
+    @State
+    private var showFormatting: Bool = false
 
-    public init(text: Binding<String>, selection: Binding<TextSelection?>) {
+    public init(text: Binding<String>, selection: Binding<TextSelection?>, toolbarBuilder: (() -> C)? = nil) {
         self._text = text
         self._selection = selection
+        self.toolbarBuilder = toolbarBuilder
     }
 
     public var body: some View {
         TextEditor(text: $text, selection: $selection)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    appropriateToolbar
+                        .popover(isPresented: $showFormatting, attachmentAnchor: .point(.topLeading)) {
+                            FormattingSheet(string: $text, selection: $selection)
+                                .presentationCompactAdaptation(.popover)
+                        }
+                }
+            }
+    }
+    
+    @ViewBuilder
+    private var appropriateToolbar: some View {
+        if let toolbarBuilder {
+            toolbarBuilder()
+        } else {
+            StandardToolber(action: {
+                withAnimation {
+                    showFormatting.toggle()
+                }
+            })
+        }
+    }
+}
+
+@available(iOS 18.0, *)
+struct StandardToolber: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Label {
+                Text("formatting")
+            } icon: {
+                Image(systemName: "textformat")
+            }
+        })
     }
 }
 
@@ -30,6 +74,6 @@ public struct MarkdownEditorView: View {
     var string: String = ""
     @Previewable @State
     var selection: TextSelection?
-    MarkdownEditorView(text: $string, selection: $selection)
+    MarkdownEditorView<StandardToolber>(text: $string, selection: $selection)
 }
 
