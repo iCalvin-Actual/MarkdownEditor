d4062d695c065541e0d7ab1e03f1fdb6bf6f9203
Calvin <me@iCalvin.dev>
2024-10-13 11:04:56 -0400

    More commands

--- Sources/MarkdownEditor/CommonCommands.swift
+++ Sources/MarkdownEditor/CommonCommands.swift
@@ -8,8 +8,28 @@
 import Foundation
 
 public struct BoldCommand: MarkdownCommand {
+    public let startMarker = "*"
+    public let endMarker = "*"
+    
+    public init() {}
+}
+public struct StrongCommand: MarkdownCommand {
     public let startMarker = "**"
     public let endMarker = "**"
+    
+    public init() {}
+}
+
+public struct ItalicCommand: MarkdownCommand {
+    public let startMarker = "_"
+    public let endMarker = "_"
+    
+    public init() {}
+}
+
+public struct Striketrhgouh: MarkdownCommand {
+    public let startMarker = "~"
+    public let endMarker = "~"
     
     public init() {}
 }
